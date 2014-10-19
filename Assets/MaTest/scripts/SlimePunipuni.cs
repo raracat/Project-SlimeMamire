6cb5b2fa650be81f06a50f4ff2cf63a33419ea73
gogoty <gogoty.gogoty.gogoty@gmail.com>
2014-10-19 12:28:07 +0900

    itweenの追加、toonshaderの追加、slimeの追加
    ・アセットのitweenを追加しました
    ・toonshaderをインポートしました
    ・MaTestの中にslimeのオブジェクトデータ、テクスチャ、Material、PhysicMaterial、スクリプトの追加をしました

---
+++ Assets/MaTest/scripts/SlimePunipuni.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimePunipuni : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.ScaleTo(gameObject, iTween.Hash(
+			"x", 1.1f, 
+			"y", 0.8f,
+			"z", 1.1f,
+			"time", 1.0f,
+			"easeType", 
+			"easeInOutExpo", 
+			"loopType", 
+			"pingPong"
+			));
+		//iTween.ScaleTo(gameObject, iTween.Hash("y", 1.5, "time", 1.0f, "looptype", iTween.LoopType.loop));
+		//iTween.ScaleTo(gameObject, iTween.Hash("z", 1.5, "time", 1.0f, "looptype", iTween.LoopType.loop));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
